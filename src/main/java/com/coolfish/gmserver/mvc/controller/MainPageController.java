fc78e4053f3998afbfbee6640f957c4af59ba723
Xiaodong <lxiaodng0206@gmail.com>
2017-03-12 10:35:19 +0800

    main panel

--- src/main/java/com/coolfish/gmserver/mvc/controller/MainPageController.java
+++ src/main/java/com/coolfish/gmserver/mvc/controller/MainPageController.java
@@ -5,31 +5,32 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.coolfish.gmserver.common.UserMananger;
-import com.coolfish.gmserver.config.Page;
 import com.coolfish.gmserver.mvc.model.User;
 import com.coolfish.gmserver.mvc.service.UserService;
 /**
  * @author Forrest
  * @date 2017年2月20日
  */
-@Controller
+@RestController
 public class MainPageController {
  
 	@Autowired
     private UserService userService;
     /**
-     * 返回结果为视图文件路径。视图相关文件默认放置在路径 resource/templates下
+     * 客户端收到json数据
      * @param name
      * @param model
      * @return
      */
     @RequestMapping("/user")
-    public String mainPage(HttpSession session, Model model) {
+    @ResponseBody
+    public List<User> mainPage(HttpSession session, Model model) {
     	int id = UserMananger.getInstance().getUserId(session);
     	User user = userService.selectById(id);
     	if(user == null) {
@@ -37,7 +38,7 @@ public class MainPageController {
     	}
     	List<User>users = userService.select();
     	model.addAttribute("users", users);
-    	return Page.User.getHtmlPath();
+    	return users;
     }
     
 }
